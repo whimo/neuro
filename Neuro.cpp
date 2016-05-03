ae7c5fce751abd77cdd7f44d57d51df316752ed0
whimo <whitemoustache01@gmail.com>
2016-05-03 22:11:00 +0300

    Improved code readability and fix some issues
    Added new comments and folders for better code readability. Also changed
    the input type and fixed some other issues

--- Neuro.cpp
+++ Neuro.cpp
@@ -1,47 +1,90 @@
+/**********************************************************
+ * Neuro.cpp                                              *
+ * A simple neuron web for character recognition          *
+ * IMPLEMENTATION                                         *
+ *                                                        *
+ * Author: Artemy Belousov (aka whitemoustache, WM, whimo)*
+ **********************************************************/
+
 #include "Neuro.h"
 
-Neuron::Neuron (size_t sx, size_t sy, int** in): x (sx), y (sy)
+int**  matrix_init (int**  matrix, size_t x, size_t y); // |Provide memory|
+bool** matrix_init (bool** matrix, size_t x, size_t y); // | for a matrix |
+
+Neuron::Neuron (size_t sx, size_t sy, bool** in): x (sx), y (sy) //The constructor
 {
     matrix_init (weight, x, y);
     matrix_init (mul   , x, y);
     matrix_init (input , x, y);
 
+    set_input (in);
+}
+
+void Neuron::set_input (bool **in) //Set the input values
+{
     for (size_t i = 0; i < x; i++)
         for (size_t j = 0; j < y; j++)
             input [i][j] = in [i][j]; //Copy the input
 }
 
-void Neuron::zoom ()
+//{================ Processing methods ================
+
+void Neuron::zoom () //Zoom input values by weights
 {
     for (size_t i = 0; i < x; i++)
         for (size_t j = 0; j < y; j++)
             mul [i][j] = input [i][j] * weight [i][j];
 }
 
-void Neuron::ssum()
+void Neuron::sum_up () //Sum up the zoomed values
 {
     for (size_t i = 0; i < x; i++)
         for (size_t j = 0; j < y; j++)
             sum += mul [i][j];
 }
 
-bool Neuron::rez() {return sum >= limit;}
+bool Neuron::rez () {return sum >= limit;} //Compare sum and limit
 
-void Neuron::w_inc()
+//}====================================================
+
+
+//{================= Learning methods =================
+
+void Neuron::w_inc () //Increase weights
 {
     for (size_t i = 0; i < x; i++)
         for (size_t j = 0; j < y; j++)
             weight [i][j] += input [i][j];
 }
 
-void Neuron::w_dec()
+void Neuron::w_dec () //Reduce weights
 {
     for (size_t i = 0; i < x; i++)
         for (size_t j = 0; j < y; j++)
             weight [i][j] -= input [i][j];
 }
 
-int main ()
+//}====================================================
+
+
+//{================ Auxiliary methods =================
+
+int** matrix_init (int** matrix, size_t x, size_t y)
 {
-    return 0;
+    matrix = new int* [x];
+    for (size_t i = 0; i < x; i++)
+        matrix [i] = new int [y];
+
+    return matrix;
 }
+
+bool** matrix_init (bool** matrix, size_t x, size_t y)
+{
+    matrix = new bool* [x];
+    for (size_t i = 0; i < x; i++)
+        matrix [i] = new bool [y];
+
+    return matrix;
+}
+
+//}====================================================
--- Neuro.h
+++ Neuro.h
@@ -1,36 +1,45 @@
-#include <stdio.h>
+/**********************************************************
+ * Neuro.h                                                *
+ * A simple neuron web for character recognition          *
+ *                                                        *
+ * Author: Artemy Belousov (aka whitemoustache, WM, whimo)*
+ **********************************************************/
 
-int** matrix_init (int** matrix, size_t x, size_t y); //Provide memory for a matrix
+#ifndef NEURO_H
+#define NEURO_H
+
+#include <iostream>
 
 class Neuron
 {
     private:
-        int** input;  //Stores synaps input
-        int** weight; //Stores synaps weights
-        int** mul;    //Stores input zoomed by the synaps weights
+        bool** input;  //Store synaps input
+        int**  weight; //Store synaptic weights
+        int**  mul;    //Store input zoomed by weights
 
         size_t x; //Number of columns
         size_t y; //Number of rows
 
-        int limit = 9; //Sum limit (chosen experimentally)
-        int sum;       //Sum of the signals
+        int limit = 9; //Sum limit (chosen experimentally for 3x5 bitmap)
+        int sum   = 0; //Sum of the signals
 
     public:
-        Neuron (size_t new_x, size_t new_y, int** in);
+        Neuron (size_t new_x, size_t new_y, bool** in); //The constructor
 
-        void zoom ();
-        void ssum ();
-        bool rez  ();
+        void set_input (bool** in); //Set the input values
 
-        void w_inc ();
-        void w_dec ();
+        //{================ Processing methods ================
+        void zoom   (); //Zoom input values by weights
+        void sum_up (); //Sum up the zoomed values
+        bool rez    (); //Compare sum and limit
+        //}====================================================
+
+        //{================= Learning methods =================
+        void w_inc (); //Increase weights
+        void w_dec (); //Reduce weights
+        //}====================================================
+
+        //TODO: add memory file
 };
 
-int** matrix_init (int** matrix, size_t x, size_t y)
-{
-    matrix = new int* [x];
-    for (size_t i = 0; i < x; i++)
-        matrix [i] = new int [y];
-
-    return matrix;
-}
+#endif //NEURO_H
