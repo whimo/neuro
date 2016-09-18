83304c21fa5441a08249d9295c5b132b3d456ff5
whimo <whitemoustache01@gmail.com>
2016-09-18 20:02:21 +0300

    It works!
    Now the libraries really work. Read the comments!

---
+++ Bitmap.cpp
@@ -0,0 +1,54 @@
+/***********************************************************
+ * Bitmap.h                                                *
+ * Bitmap reading library                                  *
+ * IMPLEMENTATION                                          *
+ *                                                         *
+ * Author: Artemy Belousov (aka whitemoustache, WM, whimo) *
+ ***********************************************************/
+
+#include "Bitmap.h"
+
+Bitmap::Bitmap (const char* filename) //The constructor
+{
+    FILE* bmp = fopen (filename, "rb"); //Open the bitmap
+
+    if (bmp == NULL)
+        throw (char*) "Argument Exception";
+
+    uchar_t header [header_size];
+    fread (header, uchar_size, header_size, bmp); //Read the header
+
+    width  = *(int*) &header [18]; // | Extract image width  |
+    height = *(int*) &header [22]; // |and height from header|
+
+    size_t row_size = (width*3 + 3) & (~3); //BMP format adds 3 zero bytes after every pixels row. Remove them
+
+    //{============== Initialize the bitmap ===============
+    bitmap = new pixel* [height];
+    for (size_t i = 0; i < height; i++)
+        bitmap [i] = new pixel [width];
+    //}====================================================
+
+    //{=================== Read pixels ====================
+    uchar_t* row = new uchar_t [row_size];
+
+	for (int32_t i = height - 1; i >= 0; i--)
+	{
+		fread (row, uchar_size, row_size, bmp); //Read the pixels row by row
+
+		for (size_t j = 0; j < width*3; j += 3)
+		{
+			(bitmap [j/3][i]).red   = row [j + 2]; //| BMP format stores pixels as |
+			(bitmap [j/3][i]).green = row [j + 1]; //| sets of 3 bytes:            |
+			(bitmap [j/3][i]).blue  = row [j];     //| 1 byte of each color        |
+		}
+	}
+	//}===================================================
+}
+
+pixel Bitmap::get_pixel (size_t x, size_t y) {return bitmap [x][y];} //Returns color of the pixel at (x;y)
+
+//{================== "Get"-methods ===================
+size_t Bitmap::get_width  () {return width; } //Get image width
+size_t Bitmap::get_height () {return height;} //Get image height
+//}====================================================
--- Neuro.cpp
+++ Neuro.cpp
@@ -1,42 +1,97 @@
-/**********************************************************
- * Neuro.cpp                                              *
- * A simple neuron web for character recognition          *
- * IMPLEMENTATION                                         *
- *                                                        *
- * Author: Artemy Belousov (aka whitemoustache, WM, whimo)*
- **********************************************************/
+/***********************************************************
+ * Neuro.cpp                                               *
+ * A simple neuron web for character recognition           *
+ * IMPLEMENTATION                                          *
+ *                                                         *
+ * Author: Artemy Belousov (aka whitemoustache, WM, whimo) *
+ ***********************************************************/
 
 #include "Neuro.h"
 
-int**  matrix_init (int**  matrix, size_t x, size_t y); // |Provide memory|
-bool** matrix_init (bool** matrix, size_t x, size_t y); // | for a matrix |
-
-Neuron::Neuron (size_t sx, size_t sy, bool** in): x (sx), y (sy) //The constructor
+Neuron::Neuron (char new_ch, size_t new_x, size_t new_y): x (new_x), y (new_y), ch (new_ch) //The constructor
 {
-    matrix_init (weight, x, y);
-    matrix_init (mul   , x, y);
-    matrix_init (input , x, y);
-
-    set_input (in);
+    weights.resize (x);
+    for (size_t i = 0; i < x; i++)
+        weights [i].resize (y, 0); //Initialize the weights vector
 }
 
-void Neuron::set_input (bool **in) //Set the input values
+
+bool Neuron::process (char real /*='\0'*/ )
+{
+    zoom   (); //Zoom input by synaptic weights...
+    sum_up (); //...and sum them up
+
+    bool result = rez ();
+
+    if (real != '\0')
+    {
+        //{===================== Learning =====================
+        //If the answer is right, do nothing
+        //If the wrong answer is "true":
+        if (ch != real && result == true)
+        {
+            w_dec (); //Substract input values from weights
+            return process (real); //Learn recursively
+        }
+
+        //If the wrong answer is "false":
+        if (ch == real && result == false)
+        {
+            w_inc (); //Add input values to weights
+            return process (real); //Learn recursively
+        }
+        //}====================================================
+    }
+
+    return result;
+}
+
+
+//{================== "Set"-methods ===================
+
+void Neuron::set_input (std::vector < std::vector <bool> > in) //Set input values
+{
+    input = in; //Copy input
+}
+
+void Neuron::set_weights (std::vector < std::vector <int> > in) //Set saved weights
 {
     for (size_t i = 0; i < x; i++)
         for (size_t j = 0; j < y; j++)
-            input [i][j] = in [i][j]; //Copy the input
+            weights [i][j] = in [i][j];
+}
+
+//}====================================================
+
+void Neuron::get_weights (std::vector < std::vector <int> >* out) //Get synaptic weights
+{
+    (*out).clear ();
+
+    for (size_t i = 0; i < y; i++)
+    {
+        std::vector <int> temp;
+        for (size_t j = 0; j < x; j++)
+            temp.push_back (weights [i][j]);
+
+        (*out).push_back (temp);
+    }
 }
 
 //{================ Processing methods ================
 
 void Neuron::zoom () //Zoom input values by weights
 {
+    mul.resize (x);
     for (size_t i = 0; i < x; i++)
+    {
+        mul [i].resize (y);
         for (size_t j = 0; j < y; j++)
-            mul [i][j] = input [i][j] * weight [i][j];
+            mul [i][j] = (bool) input [i][j] * weights [i][j];
+
+    }
 }
 
-void Neuron::sum_up () //Sum up the zoomed values
+void Neuron::sum_up () //Sum up zoomed values
 {
     for (size_t i = 0; i < x; i++)
         for (size_t j = 0; j < y; j++)
@@ -54,37 +109,14 @@ void Neuron::w_inc () //Increase weights
 {
     for (size_t i = 0; i < x; i++)
         for (size_t j = 0; j < y; j++)
-            weight [i][j] += input [i][j];
+            weights [i][j] += input [i][j];
 }
 
 void Neuron::w_dec () //Reduce weights
 {
     for (size_t i = 0; i < x; i++)
         for (size_t j = 0; j < y; j++)
-            weight [i][j] -= input [i][j];
-}
-
-//}====================================================
-
-
-//{================ Auxiliary methods =================
-
-int** matrix_init (int** matrix, size_t x, size_t y)
-{
-    matrix = new int* [x];
-    for (size_t i = 0; i < x; i++)
-        matrix [i] = new int [y];
-
-    return matrix;
-}
-
-bool** matrix_init (bool** matrix, size_t x, size_t y)
-{
-    matrix = new bool* [x];
-    for (size_t i = 0; i < x; i++)
-        matrix [i] = new bool [y];
-
-    return matrix;
+            weights [i][j] -= input [i][j];
 }
 
 //}====================================================
